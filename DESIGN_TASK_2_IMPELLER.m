49d503b8e474659ed5ac9f2e0f420774e04139eb
Diego18Ars <82908015+Diego18Ars@users.noreply.github.com>
2025-05-01 13:10:15 +0100

    Add files via upload

---
+++ DESIGN_TASK_2_IMPELLER.m
@@ -0,0 +1,15 @@
+% Bellow all values are in SI units (when units are applicable)
+p_r = 3                         % Pressure ratio
+m_flow = 2                      % Mass flow
+omega = 1000 * (2*pi)/60        % Angular velocity 
+z = 50                          % Number of blades
+b1 = 0.01                       % Initial blade height
+r_i = 0.01                      % Inner radius
+r_e = 0.05                      % Outer radius
+eta = 0.75                      % Efficiency
+
+gamma = 1.4                     % Ratio of specific heats
+T_initial = 309.34              % Initial temperature
+rho_initial = 1.28              % Initial air density
+R_air = 287                     % Perfect gas constant for air (in J/(kg.K))
+P_0 = 1*10^5                    % Initial stagnation pressure
